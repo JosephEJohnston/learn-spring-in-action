ac5450ffcc04021dfa163e81bc12194ce6171404
JosephEJohnston <1193347519@qq.com>
2023-02-09 09:32:49 +0800

    spring data jpa

--- src/main/java/com/noob/tacocloud/dao/OrderRepository.java
+++ src/main/java/com/noob/tacocloud/dao/OrderRepository.java
@@ -3,6 +3,8 @@ package com.noob.tacocloud.dao;
 import com.noob.tacocloud.model.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+import java.util.List;
 
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
 }
--- src/main/java/com/noob/tacocloud/model/Taco.java
+++ src/main/java/com/noob/tacocloud/model/Taco.java
@@ -1,19 +1,24 @@
 package com.noob.tacocloud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
-@Data
-@Table
+@Getter
+@Setter
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt = new Date();
@@ -22,7 +27,7 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @ManyToMany
     private List<IngredientRef> ingredients;
 }
