9721707ebdb1c67dc347b0d21932338a23a9465f
JosephEJohnston <1193347519@qq.com>
2023-02-09 11:24:02 +0800

    mongodb 能启动版本

--- src/main/java/com/noob/tacocloud/model/IngredientRef.java
+++ src/main/java/com/noob/tacocloud/model/IngredientRef.java
@@ -1,18 +1,11 @@
 package com.noob.tacocloud.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
 
 @Getter
 @Setter
 @ToString
-@Entity
 public class IngredientRef {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String ingredient;
--- src/main/java/com/noob/tacocloud/model/TacoUDT.java
+++ src/main/java/com/noob/tacocloud/model/TacoUDT.java
@@ -1,12 +1,10 @@
 package com.noob.tacocloud.model;
 
 import lombok.Data;
-import org.springframework.data.cassandra.core.mapping.UserDefinedType;
 
 import java.util.List;
 
 @Data
-@UserDefinedType("taco")
 public class TacoUDT {
     private final String name;
     private final List<IngredientUDT> ingredients;
