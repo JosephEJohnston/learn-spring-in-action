b7635c895e818d88163c814d9ca2491bbce6bff1
JosephEJohnston <1193347519@qq.com>
2023-02-09 14:28:53 +0800

    针对内嵌mongodb修改，实际无法使用

--- src/main/java/com/noob/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/noob/tacocloud/TacoCloudApplication.java
@@ -2,8 +2,12 @@ package com.noob.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackageClasses = TacoCloudApplication.class)
 @SpringBootApplication
+@EnableMongoAuditing
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
