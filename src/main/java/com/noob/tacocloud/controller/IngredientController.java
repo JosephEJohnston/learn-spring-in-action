9718092a4ceb75db3551dcf6c5b2e075f72ce710
JosephEJohnston <1193347519@qq.com>
2023-02-10 16:59:26 +0800

    保存，开始拆分项目

---
+++ src/main/java/com/noob/tacocloud/controller/IngredientController.java
@@ -0,0 +1,38 @@
+package com.noob.tacocloud.controller;
+
+import com.noob.tacocloud.dao.IngredientRepository;
+import com.noob.tacocloud.model.Ingredient;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
+public class IngredientController {
+
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    @PostMapping
+    @PreAuthorize("#{hasRole('ADMIN')}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("#{hasRole('ADMIN')}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        ingredientRepository.deleteById(ingredientId);
+    }
+}
