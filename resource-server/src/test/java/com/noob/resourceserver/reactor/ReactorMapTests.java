254ab863f870a7bd59293017e83155c35a4e35c0
JosephEJohnston <1193347519@qq.com>
2023-02-22 16:51:24 +0800

    11.3.3 转换和过滤

---
+++ resource-server/src/test/java/com/noob/resourceserver/reactor/ReactorMapTests.java
@@ -0,0 +1,58 @@
+package com.noob.resourceserver.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReactorMapTests {
+
+    @Test
+    public void map() {
+        Flux<Player> playerFlux = Flux
+                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                .map(n -> {
+                    String[] split = n.split("\\s");
+                    return new Player(split[0], split[1]);
+                });
+
+        StepVerifier.create(playerFlux)
+                .expectNext(new Player("Michael", "Jordan"))
+                .expectNext(new Player("Scottie", "Pippen"))
+                .expectNext(new Player("Steve", "Kerr"))
+                .verifyComplete();
+
+    }
+
+    private record Player(String firstName, String lastName) { }
+
+    @Test
+    public void flatMap() {
+        Flux<Player> playerFlux = Flux
+                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                // flatMap 可以在并行线程中完成工作
+                // 但发布顺序未知
+                .flatMap(n -> Mono.just(n)
+                        .map(p -> {
+                            String[] split = p.split("\\s");
+                            return new Player(split[0], split[1]);
+                        })
+                        .subscribeOn(Schedulers.parallel())
+                );
+
+        List<Player> playerList = Arrays.asList(
+                new Player("Michael", "Jordan"),
+                new Player("Scottie", "Pippen"),
+                new Player("Steve", "Kerr"));
+
+        StepVerifier.create(playerFlux)
+                .expectNextMatches(playerList::contains)
+                .expectNextMatches(playerList::contains)
+                .expectNextMatches(playerList::contains)
+                .verifyComplete();
+    }
+}
