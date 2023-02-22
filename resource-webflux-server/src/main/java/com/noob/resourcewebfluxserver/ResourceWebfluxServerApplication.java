d2c3385bea06021ec82c9df987b0269d255afc1d
JosephEJohnston <1193347519@qq.com>
2023-02-22 17:36:39 +0800

    迁移部分依赖

---
+++ resource-webflux-server/src/main/java/com/noob/resourcewebfluxserver/ResourceWebfluxServerApplication.java
@@ -0,0 +1,13 @@
+package com.noob.resourcewebfluxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceWebfluxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceWebfluxServerApplication.class, args);
+    }
+
+}
---
+++ resource-webflux-server/src/test/java/com/noob/resourcewebfluxserver/ResourceWebfluxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.resourcewebfluxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceWebfluxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
