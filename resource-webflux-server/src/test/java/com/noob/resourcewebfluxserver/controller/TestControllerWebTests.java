f773996aab77082f62485080e7cedab7a53c386f
JosephEJohnston <1193347519@qq.com>
2023-02-23 10:22:28 +0800

    项目能启动，但 jpa 和 reactive 不兼容

---
+++ resource-webflux-server/src/test/java/com/noob/resourcewebfluxserver/controller/TestControllerWebTests.java
@@ -0,0 +1,31 @@
+package com.noob.resourcewebfluxserver.controller;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestControllerWebTests {
+
+    @Resource
+    private WebTestClient testClient;
+
+    @Test
+    public void shouldReturnRecentTacos() {
+        testClient.get().uri("/api/tacos?recent")
+                .accept(MediaType.APPLICATION_JSON).exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$").isArray()
+                .jsonPath("$.length()").isEqualTo(3)
+                .jsonPath("$[?(@.name == 'Carnivore')]").exists()
+                .jsonPath("$[?(@.name == 'Bovine Bounty')]").exists()
+                .jsonPath("$[?(@.name == 'Veg-Out')]").exists();
+    }
+
+}
