7c23088ef3e520f54498aac3da3f3693874c346d
JosephEJohnston <1193347519@qq.com>
2023-02-23 10:27:59 +0800

    12.3 测试反应式控制器，done

--- resource-webflux-server/src/main/java/com/noob/resourcewebfluxserver/dao/TacoRepository.java
+++ resource-webflux-server/src/main/java/com/noob/resourcewebfluxserver/dao/TacoRepository.java
@@ -3,6 +3,7 @@ package com.noob.resourcewebfluxserver.dao;
 import com.noob.commons.model.Taco;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+// 这玩意其实不能用，jpa 和 reactive 不兼容，作者老哥可能根本没测试过
 public interface TacoRepository extends ReactiveCrudRepository<Taco, Long> {
 
 }
