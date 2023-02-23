820b7977c46ea8909243255930fb63df52c89609
JosephEJohnston <1193347519@qq.com>
2023-02-23 17:33:46 +0800

    15.2.2/3

--- resource-webflux-server/src/main/java/com/noob/resourcewebfluxserver/ResourceWebfluxServerApplication.java
+++ resource-webflux-server/src/main/java/com/noob/resourcewebfluxserver/ResourceWebfluxServerApplication.java
@@ -1,10 +1,19 @@
 package com.noob.resourcewebfluxserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ResourceWebfluxServerApplication {
+
+    // /httptrace 改为 /httpexchanges
+    @Bean
+    public HttpExchangeRepository httpTraceRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceWebfluxServerApplication.class, args);
