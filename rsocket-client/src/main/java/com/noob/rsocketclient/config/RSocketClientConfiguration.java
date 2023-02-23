0a0e1083cb4ee69a343e1ab5b7f66bb889e567f1
JosephEJohnston <1193347519@qq.com>
2023-02-23 16:06:54 +0800

    rsocket 成功发送接收消息

---
+++ rsocket-client/src/main/java/com/noob/rsocketclient/config/RSocketClientConfiguration.java
@@ -0,0 +1,26 @@
+package com.noob.rsocketclient.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+@Configuration
+@Slf4j
+public class RSocketClientConfiguration {
+
+    @Bean
+    public ApplicationRunner sender(RSocketRequester.Builder requesterBuilder) {
+        return args -> {
+            RSocketRequester tcp = requesterBuilder.tcp("localhost", 7000);
+
+            String who = "Craig";
+            tcp.route("greeting/{name}", who)
+                    .data("Hello RSocket!")
+                    .retrieveMono(String.class)
+                    .subscribe(response -> log.info("Got a response: {}", response));
+        };
+    }
+
+}
