df130799fca07bc102323ea5939f0a7e8d1abf3d
JosephEJohnston <1193347519@qq.com>
2023-02-23 15:50:18 +0800

    rsocket init

---
+++ rsocket-client/src/test/java/com/noob/rsocketclient/RsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.rsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
