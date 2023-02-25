b6e9bc5f6549e663c6c441c66f14cc5828c47928
JosephEJohnston <1193347519@qq.com>
2023-02-25 20:13:29 +0800

    admin 可以登录

--- admin-server/src/main/java/com/noob/adminserver/config/SecurityConfig.java
+++ admin-server/src/main/java/com/noob/adminserver/config/SecurityConfig.java
@@ -1,13 +1,15 @@
 package com.noob.adminserver.config;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -19,26 +21,30 @@ public class SecurityConfig {
         serverHttpSecurity
                 .authorizeExchange(auth -> auth
                         .pathMatchers("/login", "/logout", "/error").permitAll()
-                        .pathMatchers("/variables.css", "/sba-settings.js", "/assets/**").permitAll()
+                        .pathMatchers("/actuator/info", "/actuator/health").permitAll()
+                        .pathMatchers("/variables.css", "/sba-settings.js",
+                                "/assets/**", "/favicon.ico").permitAll()
                         //.pathMatchers("/actuator/**", "/applications", "/", "/instances", "/instances/**").authenticated()
+                        .anyExchange().hasAnyRole("USER")
 
                         // 很难区分哪些需要鉴权还是不用鉴权
-                        .anyExchange().permitAll()
+                        //.anyExchange().permitAll()
                 )
                 .formLogin()
                 .and()
+                .httpBasic(Customizer.withDefaults())
                 .csrf().disable();
 
         return serverHttpSecurity.build();
     }
 
-    /*@Bean
-    public UserDetailsService users() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{bcrypt}53cr3t")
-                .roles("USER", "ADMIN")
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(admin);
-    }*/
+        return new MapReactiveUserDetailsService(user);
+    }
 }
