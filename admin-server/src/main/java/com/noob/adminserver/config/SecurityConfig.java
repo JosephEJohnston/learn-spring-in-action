bf5a89fa82cd44f03f5273be48bdbda57bc4ebcc
JosephEJohnston <1193347519@qq.com>
2023-02-25 17:23:28 +0800

    admin server 问题还是不少的

--- admin-server/src/main/java/com/noob/adminserver/config/SecurityConfig.java
+++ admin-server/src/main/java/com/noob/adminserver/config/SecurityConfig.java
@@ -1,17 +1,28 @@
 package com.noob.adminserver.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(auth -> auth
+                        /*.pathMatchers("/login", "/logout", "/error", "/instances").permitAll()
+                        .pathMatchers("/variables.css", "/sba-settings.js", "/assets/**").permitAll()
+                        .pathMatchers("/actuator/**", "/applications", "/").authenticated()*/
+
+                        // 很难区分哪些需要鉴权还是不用鉴权
+                        .anyExchange().permitAll())
+                /*.formLogin()
+                .and()*/
                 .csrf().disable();
 
-        return httpSecurity.build();
+        return serverHttpSecurity.build();
     }
 }
